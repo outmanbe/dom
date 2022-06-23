cd0333b589e9742216490d06c1d09dd36df1ec29
Out-be <outman.be@live.fr>
2022-06-23 14:27:24 +0200

    first commit

---
+++ dom.js
@@ -0,0 +1,4 @@
+function checkForm(f) {
+    alert("Vous vous appelez : " + f.elements['nom'].value + " " + f.elements['prenom'].value + " et vous avez : " + " " + f.elements['âge'].value + " ans " );
+    return false;           
+}
